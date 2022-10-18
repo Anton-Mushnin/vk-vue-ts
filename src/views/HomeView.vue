5e04e49f800df17323bc68f9b0960a7d685066ed
Anton Mushnin <anton.mushnin@gmail.com>
2022-10-18 10:56:54 +0300

    new auth path

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,16 +2,30 @@
 import TheUsers from "../components/TheUsers.vue";
 import TheFriends from "../components/TheFriends.vue";
 import { useAuthStore } from "@/stores/AuthStore";
+import router from '@/router';
+
 
 
 const authStore = useAuthStore();
+// const authStore = useAuthStore();
+const parsedHash = new URLSearchParams(
+  window.location.hash.substring(1)
+);
+
+if (parsedHash.has("access_token")) {
+  authStore.tokenRequestSuccess(parsedHash.get("access_token")!);
+  router.push('/');
+}
+
+
+
 </script>
 
 
 <template>
   <main>
     <div class="users-container">
-      <a v-if="!authStore.userToken" href="https://oauth.vk.com/authorize?client_id=51451394&display=page&redirect_uri=https://anton-mushnin.github.io/vk-vue-ts/auth&scope=friends&response_type=token&v=5.131&state=123456">
+      <a v-if="!authStore.userToken" href="https://oauth.vk.com/authorize?client_id=51451394&display=page&redirect_uri=https://anton-mushnin.github.io/vk-vue-ts&scope=friends&response_type=token&v=5.131&state=123456">
         login
       </a>
       <TheUsers />
